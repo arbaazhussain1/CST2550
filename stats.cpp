86a2d9384bfc8b1d87c3b79c549102a94193b7da
Arbaaz <arbaazahussain786@gmail.com>
2023-11-16 10:51:08 +0000

    initial commit

---
+++ stats.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+//using namespace std::
+int main()
+{
+ //   int x,y;
+ //   std:: cout << "Please enter two numbers:";
+ //   std::cin >> x >> y; // +
+ //   std::cout << "The sum of " << x << "and" << y
+ //             << "is: " << x + y;
+
+
+  //  int x,y;
+  //  std:: cout << "Please enter two numbers:";
+  //  std::cin  >> x >> y;
+  //  std::cout << "The difference between " << x << "and" << y
+  //            << "is: " << x - y;
+
+ //  int x,y;
+ //     std:: cout << "Please enter two numbers:";
+ //     std::cin >> x >> y; // +
+ //     std::cout << "The product of " << x << "and" << y
+ //               << "is: " << x * y;
+
+
+   //    int x,y;
+   //    std:: cout << "Please enter two numbers:";
+   //    std::cin >> x >> y; // +
+   //    std::cout << "The distance between  " << x << "and" << y
+   //            << "is: " << y - x;
+
+      int x,y;
+      float mean, sum;
+
+       std:: cout << "Please enter two numbers:";
+       std::cin >> x >> y; // +
+       sum = (x + y);
+       mean = (sum/2);
+       std::cout << "The mean average " << x << "and" << y
+            //     << "is: " << ((me)/2);
+            << "is" << ( (sum) /2 );
+
+    return 0;
+
+
+}
+
